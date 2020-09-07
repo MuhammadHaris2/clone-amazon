0e703bef83d21941900bc8a5113a11a93540c1f9
MuhammadHaris2 <mhari8339@gmail.com>
2020-09-07 08:57:48 +0500

    Updates

---
+++ precache-manifest.47d64dbfb382ed327b4e185c39177ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a666c30d74c2d2e4a8966d727598d87b",
+    "url": "/clone-amazon/index.html"
+  },
+  {
+    "revision": "deaf28420abdafed093f",
+    "url": "/clone-amazon/static/css/main.00178b92.chunk.css"
+  },
+  {
+    "revision": "e7fb6d1c4116a59ff46d",
+    "url": "/clone-amazon/static/js/2.2d7fe3d8.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/clone-amazon/static/js/2.2d7fe3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deaf28420abdafed093f",
+    "url": "/clone-amazon/static/js/main.9249e2c3.chunk.js"
+  },
+  {
+    "revision": "42e8c1c4f77ae6629e2e",
+    "url": "/clone-amazon/static/js/runtime-main.cf7ada63.js"
+  }
+]);
